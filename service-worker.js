3e5b2b0c009658de61ca1e9b1f6bed10c4530e98
Trần Quốc Hưng <43161982+Tranquochung97@users.noreply.github.com>
2026-09-10 07:25:34 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pmp-coach-iphone-v30-final-eco2026';
+const CACHE='pmp-coach-final-rfg10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
